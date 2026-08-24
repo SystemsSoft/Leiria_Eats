8d7f0929b72b3a96c8c2c504a60552b143147d0a
bruno <devmob60@gmail.com>
2026-08-24 18:24:21 -0300

    adiciona subpedidos

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -96,8 +96,8 @@ android {
         applicationId = "org.leria.eats.project"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 4
-        versionName = "1.3"
+        versionCode = 5
+        versionName = "1.4"
     }
     packaging {
         resources {
