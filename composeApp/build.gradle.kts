44088cdad7d0ff7cd0e2e5a4eb943ba80e552e92
bruno <devmob60@gmail.com>
2026-08-25 15:33:30 -0300

    retira favorito

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -96,8 +96,8 @@ android {
         applicationId = "org.leria.eats.project"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 5
-        versionName = "1.4"
+        versionCode = 6
+        versionName = "1.5"
     }
     packaging {
         resources {
