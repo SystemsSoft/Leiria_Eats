551d3bc4ca5498a0f16b14917e7ecc87b189306e
bruno <devmob60@gmail.com>
2026-09-25 14:52:14 -0300

    nova versão

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -103,8 +103,8 @@ android {
         applicationId = "org.leria.eats.project"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 12
-        versionName = "2.1"
+        versionCode = 13
+        versionName = "2.2"
     }
     packaging {
         resources {
